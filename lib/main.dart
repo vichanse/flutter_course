83e86e159be2b0dc3931bf21d6b81a8a89838d96
Vicky <vicky.nseng@gmail.com>
2018-10-06 03:55:33 +0200

    Adding types

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-main() {
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(

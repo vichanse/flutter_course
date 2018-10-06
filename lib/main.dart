3b335c498830ff1e1378ddab2b70d3af53c9829c
Vicky <vicky.nseng@gmail.com>
2018-10-06 03:56:21 +0200

    Shorter function notation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {

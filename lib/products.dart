be2c625556528faeb44e18dcf3639d968947214a
Vicky <vicky.nseng@gmail.com>
2018-10-09 04:36:34 +0200

    Optimizing the list loading behaviour

--- lib/products.dart
+++ lib/products.dart
@@ -3,19 +3,20 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   final List<String> products;
   Products(this.products);
+
+  Widget _buildProductItem(BuildContext context, index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text((products[index]))
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: products
-          .map((element) => Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text((element))
-                  ],
-                ),
-              ))
-          .toList(),
-    );
+    return ListView.builder(item);
   }
 }

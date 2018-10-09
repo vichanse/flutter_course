3a281381b028eeea76cc8bbe61ab0eee66521067
Vicky <vicky.nseng@gmail.com>
2018-10-09 05:08:08 +0200

    Adding multiple pages to our app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './product_manager.dart';
+import './pages/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,16 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-        accentColor: Colors.deepPurple,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple,
         ),
-        body: ProductManager(),
-      ),
-    );
+        home: HomePage());
   }
 }

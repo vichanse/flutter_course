54ee1e8680b290fde03b4932ddd3ec0e30c98861
Vicky <vicky.nseng@gmail.com>
2018-10-09 05:38:18 +0200

    Improving the look of product page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -4,9 +4,27 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Product detail'),
-        ),
-        body: Center(child: Text('On the Product page')));
+      appBar: AppBar(
+        title: Text('Product detail'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Details'),
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text('BACK'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }

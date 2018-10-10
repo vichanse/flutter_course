2544d549f664608c9c8f86d6477f8db876f8a06e
Vicky <vicky.nseng@gmail.com>
2018-10-10 06:51:04 +0200

    Adding tabs

---
+++ lib/pages/products_admin.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import './products.dart';
+import './product_create.dart';
+import './product_list.dart';
+
+class ProductsAdminPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+            child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Products list'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => ProductsPage(),
+                  ),
+                );
+              },
+            )
+          ],
+        )),
+        appBar: AppBar(
+          title: Text('Products Admin'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.create),
+                text: 'Create Products',
+              ),
+              Tab(
+                icon: Icon(Icons.list),
+                text: 'My Products',
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+            children: <Widget>[ProductCreatePage(), ProductListPage()]),
+      ),
+    );
+  }
+}

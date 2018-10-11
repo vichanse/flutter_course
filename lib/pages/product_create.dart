a77692cd643180c915e01aa0de97b4a7b28e2c30
Vicky <vicky.nseng@gmail.com>
2018-10-11 05:16:33 +0200

    Finishing the tabs navigation

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Center(
+      child: Text('Create a product'),
+    );
   }
 }
